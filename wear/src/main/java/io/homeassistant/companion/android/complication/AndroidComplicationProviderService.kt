7a520cd67420dff84680a6538490d66355f0aa32
ronmichel <40127030+ronmichel@users.noreply.github.com>
2022-02-04 14:36:50 +0100

    Update AndroidComplicationProviderService.kt

--- wear/src/main/java/io/homeassistant/companion/android/complication/AndroidComplicationProviderService.kt
+++ wear/src/main/java/io/homeassistant/companion/android/complication/AndroidComplicationProviderService.kt
@@ -17,14 +17,7 @@ package io.homeassistant.companion.android.complication
 
 import android.content.ComponentName
 import android.util.Log
-import androidx.wear.watchface.complications.data.ComplicationData
-import androidx.wear.watchface.complications.data.ComplicationType
-import androidx.wear.watchface.complications.data.LongTextComplicationData
-import androidx.wear.watchface.complications.data.PlainComplicationText
-import androidx.wear.watchface.complications.data.RangedValueComplicationData
-import androidx.wear.watchface.complications.data.ShortTextComplicationData
-import androidx.wear.watchface.complications.datasource.ComplicationRequest
-import androidx.wear.watchface.complications.datasource.SuspendingComplicationDataSourceService
+import android.support.wearable.complications.*
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import java.util.Locale
@@ -35,7 +28,7 @@ import java.util.Locale
  * Note: This class uses the suspending variation of complication data source service to support
  * async calls to the data layer, that is, to the DataStore saving the persistent values.
  */
-class AndroidComplicationDataSourceService : SuspendingComplicationDataSourceService() {
+class AndroidComplicationDataSourceService : ComplicationProviderService() {
 
     /*
      * Called when a complication has been activated. The method is for any one-time
