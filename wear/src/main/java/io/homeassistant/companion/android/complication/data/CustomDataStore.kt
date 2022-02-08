7a4f272e1274fcdffa3cee4e8859eb944f58515f
ronmichel <40127030+ronmichel@users.noreply.github.com>
2022-02-08 10:12:04 +0100

    Update CustomDataStore.kt

--- wear/src/main/java/io/homeassistant/companion/android/complication/data/CustomDataStore.kt
+++ wear/src/main/java/io/homeassistant/companion/android/complication/data/CustomDataStore.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.homeassistant.companion.android.complication
+package io.homeassistant.companion.android.complication.data
 
 import android.content.Context
 import androidx.datastore.core.DataStore
