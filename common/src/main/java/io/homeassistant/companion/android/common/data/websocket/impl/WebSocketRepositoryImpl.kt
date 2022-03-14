4a94a3ad7f0f49d3fe3906ce80c09f3b1b01269d
ronmichel <40127030+ronmichel@users.noreply.github.com>
2022-03-14 11:38:16 +0100

    Update WebSocketRepositoryImpl.kt

--- common/src/main/java/io/homeassistant/companion/android/common/data/websocket/impl/WebSocketRepositoryImpl.kt
+++ common/src/main/java/io/homeassistant/companion/android/common/data/websocket/impl/WebSocketRepositoryImpl.kt
@@ -351,7 +351,9 @@ class WebSocketRepositoryImpl @Inject constructor(
         if (eventResponseType != null && eventResponseType.isTextual) {
             val eventResponseClass = when (eventResponseType.textValue()) {
                 EVENT_STATE_CHANGED -> {
+                    if (response.event?.get("data").get("entityId") == "light.philips_eyecare") {
                     Log.d(TAG, "Event State changed stapstap")
+                    }
                     object : TypeReference<EventResponse<StateChangedEvent>>() {}
                 }
                 EVENT_AREA_REGISTRY_UPDATED ->
