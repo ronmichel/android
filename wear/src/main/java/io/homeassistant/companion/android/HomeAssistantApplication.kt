564daba02286bfaa09cb439bede43d063c147041
ronmichel <40127030+ronmichel@users.noreply.github.com>
2022-03-17 23:01:28 +0100

    Update HomeAssistantApplication.kt

--- wear/src/main/java/io/homeassistant/companion/android/HomeAssistantApplication.kt
+++ wear/src/main/java/io/homeassistant/companion/android/HomeAssistantApplication.kt
@@ -3,14 +3,28 @@ package io.homeassistant.companion.android
 import android.app.Application
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.wifi.WifiManager
+import android.os.PowerManager
 import dagger.hilt.android.HiltAndroidApp
 import io.homeassistant.companion.android.sensors.SensorReceiver
+import io.homeassistant.companion.android.websocket.WebsocketBroadcastReceiver
 
 @HiltAndroidApp
 open class HomeAssistantApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        registerReceiver(
+            WebsocketBroadcastReceiver(),
+            IntentFilter().apply {
+                addAction(Intent.ACTION_SCREEN_OFF)
+                addAction(Intent.ACTION_SCREEN_ON)
+                addAction(PowerManager.ACTION_POWER_SAVE_MODE_CHANGED)
+                addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION)
+                addAction(WifiManager.WIFI_STATE_CHANGED_ACTION)
+            }
+        )        
+        
         val sensorReceiver = SensorReceiver()
         // This will cause the sensor to be updated every time the OS broadcasts that a cable was plugged/unplugged.
         // This should be nearly instantaneous allowing automations to fire immediately when a phone is plugged
