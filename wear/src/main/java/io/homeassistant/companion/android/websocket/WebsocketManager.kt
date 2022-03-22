a9751858bedc42f5ee85ffac267eea1f9a9551a1
ronmichel <40127030+ronmichel@users.noreply.github.com>
2022-03-22 10:00:30 +0100

    Update WebsocketManager.kt

--- wear/src/main/java/io/homeassistant/companion/android/websocket/WebsocketManager.kt
+++ wear/src/main/java/io/homeassistant/companion/android/websocket/WebsocketManager.kt
@@ -20,7 +20,7 @@ import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
-import com.joaomgcd.common.tasker.*
+import com.joaomgcd.common.tasker.AutoAppsThirdParty
 import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
 import io.homeassistant.companion.android.BuildConfig
