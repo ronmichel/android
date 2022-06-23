f99b53578806d44e253f159ae5d7b84aa7a60be0
ronmichel <40127030+ronmichel@users.noreply.github.com>
2022-06-23 10:27:46 +0200

    Add files via upload

---
+++ wear/build.gradle.kts
@@ -0,0 +1,124 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("com.github.triplet.play")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "io.homeassistant.companion.android"
+        minSdk = 26
+        targetSdk = 30
+
+        versionName = System.getenv("VERSION") ?: "LOCAL"
+        // We add 1 because the app and wear versions need to have different version codes.
+        versionCode = (System.getenv("VERSION_CODE")?.toIntOrNull() ?: 1) + 1
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments(mapOf("room.incremental" to "true"))
+            }
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
+
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_11)
+        targetCompatibility(JavaVersion.VERSION_11)
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_PATH") ?: "release_keystore.keystore")
+            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: ""
+            keyAlias = System.getenv("KEYSTORE_ALIAS") ?: ""
+            keyPassword = System.getenv("KEYSTORE_ALIAS_PASSWORD") ?: ""
+            enableV1Signing = true
+            enableV2Signing = true
+        }
+    }
+
+    buildTypes {
+        named("debug").configure {
+            applicationIdSuffix = ".debug"
+        }
+        named("release").configure {
+            isDebuggable = false
+            isJniDebuggable = false
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    lint {
+        disable += "MissingTranslation"
+    }
+
+    kapt {
+        correctErrorTypes = true
+    }
+}
+
+play {
+    serviceAccountCredentials.set(file("playStorePublishServiceCredentialsFile.json"))
+    track.set("beta")
+    resolutionStrategy.set(com.github.triplet.gradle.androidpublisher.ResolutionStrategy.IGNORE)
+    commit.set(false)
+}
+
+dependencies {
+    implementation(project(":common"))
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.21")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.6.2")
+
+    implementation("com.google.android.material:material:1.6.0")
+
+    implementation("androidx.wear:wear:1.2.0")
+    implementation("com.google.android.gms:play-services-wearable:17.1.0")
+    implementation("androidx.wear:wear-input:1.2.0-alpha02")
+    implementation("androidx.wear:wear-remote-interactions:1.0.0")
+    compileOnly("com.google.android.wearable:wearable:2.9.0")
+
+    implementation("com.google.dagger:hilt-android:2.42")
+    kapt("com.google.dagger:hilt-android-compiler:2.42")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+
+    implementation("com.mikepenz:iconics-core:5.3.3")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.mikepenz:community-material-typeface:6.4.95.0-kotlin@aar")
+    implementation("com.mikepenz:iconics-compose:5.3.3")
+
+    implementation("androidx.activity:activity-ktx:1.4.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.compose.compiler:compiler:1.1.1")
+    implementation("androidx.compose.foundation:foundation:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling:1.1.1")
+    implementation("androidx.wear.compose:compose-foundation:1.0.0-alpha14")
+    implementation("androidx.wear.compose:compose-material:1.0.0-alpha14")
+    implementation("androidx.wear.compose:compose-navigation:1.0.0-alpha14")
+
+    implementation("com.google.guava:guava:31.1-android")
+    implementation("androidx.wear.tiles:tiles:1.0.1")
+
+    implementation("androidx.wear.watchface:watchface-complications-data-source-ktx:1.1.0-rc01")
+}
